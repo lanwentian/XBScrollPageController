d33cf75ab67398ac9f55de587245a94b60cd2293
changjianfeishui <3297375967@qq.com>
2015-09-08 17:15:52 +0800

    v1.0 修复由于项目重命名引发的bug

--- XBScrollPageController/ViewController.m
+++ XBScrollPageController/ViewController.m
@@ -109,15 +109,7 @@
     NSArray *classNames = @[@"TestViewController"];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-
-        
-        [self reloadDataWith:titleArray andSubViewdisplayClassNames:classNames];
-        
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self scrollToTagByIndex:6];
-        });
-        
+        [self scrollToTagByIndex:6];
     });
 
 }
